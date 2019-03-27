5e39cb59f4ef3741b28d1616f2705814ce93bb39
radriazola <radriazola@copec.cl>
2019-03-27 13:10:46 -0300

    nuevos cambio

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # resources :gas_stations
+  resources :gas_stations, except: :destroy
+  root to:'gas_stations#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
