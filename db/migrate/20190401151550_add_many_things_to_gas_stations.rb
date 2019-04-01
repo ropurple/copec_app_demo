2a7a3706085158b60d57f7a761ea88d692654972
radriazola <radriazola@copec.cl>
2019-04-01 13:02:26 -0300

    make it better3

---
+++ db/migrate/20190401151550_add_many_things_to_gas_stations.rb
@@ -0,0 +1,7 @@
+class AddManyThingsToGasStations < ActiveRecord::Migration[5.2]
+  def change
+    add_column :gas_stations, :state, :string
+    add_column :gas_stations, :phone, :string
+    add_column :gas_stations, :owner, :string
+  end
+end
